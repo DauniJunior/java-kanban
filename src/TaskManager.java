fc1f8547389b23813c14604e7e01bd3b820c8729
Максим Чарушин <rtrs_tishvin2@mail.ru>
2025-09-22 01:08:39 +0300

    Исправлены ошибки стиля

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -3,26 +3,41 @@ import java.util.List;
 public interface TaskManager {
     // Методы для задач
     List<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTaskById(int id);
+
     int addTask(Task task);
+
     void updateTask(Task updatedTask);
+
     void deleteTaskById(int id);
 
     // Методы для эпиков
     List<Epic> getAllEpics();
+
     void deleteAllEpics();
+
     Epic getEpicById(int id);
+
     int addEpic(Epic epic);
+
     void updateEpic(Epic updatedEpic);
+
     void deleteEpicById(int id);
 
     // Методы для подзадач
     List<Subtask> getAllSubTasks();
+
     void deleteAllSubTasks();
+
     Subtask getSubTaskById(int id);
+
     int addSubTask(Subtask subtask);
+
     void updateSubTask(Subtask subtask);
+
     void deleteSubTaskById(int id);
 
     // Дополнительные методы
